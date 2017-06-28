979737fbfe3484b7839dfa0db6d3ed1370383982
Ehbraheem <ehbraheem@gmail.com>
2017-06-28 08:12:17 -0700

    Espresso: Add MenuActivity Test

--- app/src/androidTest/java/com/example/android/teatime/MenuActivityScreenTest.java
+++ app/src/androidTest/java/com/example/android/teatime/MenuActivityScreenTest.java
@@ -25,8 +25,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.anything;
 
 /**
@@ -56,7 +59,15 @@ public class MenuActivityScreenTest {
 
         onData(anything())
                 .inAdapterView(withId(R.id.tea_grid_view))
+                .atPosition(2)
+                .onChildView(withId(R.id.tea_grid_name))
+                .check(matches(withText(TEA_NAME)));
+
+        onData(anything())
+                .inAdapterView(withId(R.id.tea_grid_view))
                 .atPosition(2).perform(click());
+
+        onView(withId(R.id.tea_name_text_view)).check(matches(withText(TEA_NAME)));
         
     }
 
